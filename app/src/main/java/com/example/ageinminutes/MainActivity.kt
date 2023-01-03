8796b6bccd4212f2ceee256f9867f505610b2dec
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-01-03 11:50:27 +0600

    ui finished

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.ageinminutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener{
+            Toast.makeText(this, "Button Works!", Toast.LENGTH_LONG).show()
+        }
     }
 }
